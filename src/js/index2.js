0b423e737f0e37509053525008122db6b12ebd8e
Carlos Quiros <carlosquiros12@gmail.com>
2020-12-19 22:30:37 -0500

    Cambios minimos a la vista

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', function(){
-    const slider = document.getElementById('slider');
-    let imagenes = ['imagenes/4.jpg','imagenes/3.jpg','imagenes/2.jpg','imagenes/1.jpg']
+    const imagen = document.getElementById('imagen');
+    let arrImagenes = ['imagenes/4.jpg','imagenes/3.jpg','imagenes/2.jpg','imagenes/1.jpg']
     
-    slider.src =  'imagenes/4.jpg';
+    imagen.src =  arrImagenes[3];
+    
     console.log('hola','mundo');
 })
