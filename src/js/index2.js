4bb9a124abe677699cb0bfad800e77ed18b58b6a
Carlos Quiros <carlosquiros12@gmail.com>
2020-12-19 20:39:04 -0500

    Cambiando el segundo index

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function(){
-    document.slider.src = 'src/imagenes/1.jpg';
+    const slider = document.getElementById('slider');
+    let imagenes = ['imagenes/4.jpg','imagenes/3.jpg','imagenes/2.jpg','imagenes/1.jpg']
+    
+    slider.src =  'imagenes/4.jpg';
+    console.log('hola','mundo');
 })
