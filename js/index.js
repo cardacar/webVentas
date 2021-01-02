b4a197a0043459aaec80aa290db269d9374004e7
Carlos Quiros <carlosquiros12@gmail.com>
2021-01-01 21:59:23 -0500

    Configurando peticiones get al servidor

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 //Todo el codigo relacionado al servidor
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 
 
@@ -14,6 +15,8 @@ app.set('port', process.env.PORT || 4000);
 
 
 //Middlewares
+app.use(morgan('dev'));
+
 
 
 //Global Variables
@@ -23,12 +26,23 @@ app.get('/',(req, res) =>{
     res.sendFile(path.resolve(__dirname, '../src/views/index.html'));
 });
 
+app.get('/products',(req, res) =>{
+    res.sendFile(path.resolve(__dirname, '../src/views/products.html'));
+});
+
+app.get('/login',(req, res) =>{
+    res.sendFile(path.resolve(__dirname, '../src/views/login.html'));
+});
+
+
+
+
 
 
 
 //Static Files
 app.use(express.static('src'));
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
 app.use(express.static('src/public'));
 
 
